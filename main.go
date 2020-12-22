2948c5ffad7e1a3e85bcef37cf2f66202c5dc543
Felipe Ferraz <felipe.ferraz@engenharia.ufjf.br>
2020-12-22 14:56:16 -0300

    :white_check_mark: Testing game engine

--- main.go
+++ main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"MonteCarloTreeSearchInGo/engine"
+	"fmt"
+)
+
 func main() {
+	//idx := 0
+	var game engine.Engine
+	var location engine.BoardLocation
+	game.InitializeGame()
+	location = engine.BoardLocation{X: 2, Y: 2}
+	_ = game.PlayMove(1, location)
+	location = engine.BoardLocation{X: 0, Y: 0}
+	_ = game.PlayMove(-1, location)
+	location = engine.BoardLocation{X: 1, Y: 1}
+	_ = game.PlayMove(1, location)
+	location = engine.BoardLocation{X: 1, Y: 0}
+	_ = game.PlayMove(-1, location)
+	location = engine.BoardLocation{X: 1, Y: 2}
+	_ = game.PlayMove(1, location)
+	location = engine.BoardLocation{X: 2, Y: 0}
+	_ = game.PlayMove(-1, location)
+	fmt.Println(game.CheckEnd())
 }
